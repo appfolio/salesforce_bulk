988eb988de86ab92e300d623557ea65e2bf846fa
Chris Schulte <chris@oceanbreezesoftware.com>
2013-06-17 14:41:52 -0700

    cs - Rescuing errors when parsing CSV results on job.

--- lib/salesforce_bulk/job.rb
+++ lib/salesforce_bulk/job.rb
@@ -129,19 +129,23 @@ module SalesforceBulk
     def parse_results response
       @result.success = true
       @result.raw = response.lines.to_a[1..-1].join
-      csvRows = CSV.parse(response, :headers => true)
+      begin
+        csvRows = CSV.parse(response, :headers => true)
 
-      csvRows.each_with_index  do |row, index|
-        if @@operation != "query"
-          row["Created"] = row["Created"] == "true" ? true : false
-          row["Success"] = row["Success"] == "true" ? true : false
-        end
+        csvRows.each_with_index  do |row, index|
+          if @@operation != "query"
+            row["Created"] = row["Created"] == "true" ? true : false
+            row["Success"] = row["Success"] == "true" ? true : false
+          end
 
-        @result.records.push row
-        if row["Success"] == false
-          @result.success = false 
-          @result.errors.push({"#{index}" => row["Error"]}) if row["Error"]
+          @result.records.push row
+          if row["Success"] == false
+            @result.success = false 
+            @result.errors.push({"#{index}" => row["Error"]}) if row["Error"]
+          end
         end
+      rescue Exception => ex
+        puts "Unable to parse results CSV data"
       end
 
       @result.message = "The job has been closed."
